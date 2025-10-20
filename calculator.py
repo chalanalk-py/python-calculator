7d75f0b497d32503d5104ce76b0cbb1e2286cf8b
Chalana <chalanalk10@gmail.com>
2025-10-20 11:47:22 -0700

    Version 1

---
+++ calculator.py
@@ -0,0 +1,31 @@
+def add(a,b):
+    return(a+b)
+
+def substraction(a,b):
+    return(a-b)
+
+
+
+#Main loop
+while (True):
+
+    a = int(input("Enter number 1 : "))
+    b = int(input("Enter number 2 : "))
+    print("Enter The Operations \n1.Addition (+)\n2.Substration (-)\n3.Terminate (#)")
+    operation = input("Enter operation : ")
+    
+    result = 0
+
+    if (operation=="#"):
+        break
+    
+    elif (operation=="+"):
+        result=add(a,b)
+
+    elif (operation=="-"):
+        result=substraction(a,b)
+    
+    else:
+        print("Invalid operation. Enter again.\n")
+
+    print(result)
