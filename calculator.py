a0111f4418a8afe9509e04d5db711110cffda8f0
Chalana <chalanalk10@gmail.com>
2025-10-20 17:37:22 -0700

    Version 3

--- calculator.py
+++ calculator.py
@@ -4,27 +4,44 @@ def add(a,b):
 def substraction(a,b):
     return a-b
 
+def multiply(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+
 #Main loop
 while (True):
 
-    print("==== Operations ====\n1.Addition (+)\n2.Substration (-)\n3.Terminate (#)\n")
+    print("\n==== Operations ====")
+    print("Addition (+)")
+    print("Substraction (-)")
+    print("Multiplication (*)")
+    print("Multiplication (/)")
+    print("Terminate (#)")
     operation = input("Enter operation : ")
 
     if (operation=="#"):
         break
-    
-    a = int(input("Enter number 1 : "))
-    b = int(input("Enter number 2 : "))
+
+    num1 = int(input("\nEnter number 1 : "))
+    num2 = int(input("Enter number 2 : "))
     
     result = 0
     
     if (operation=="+"):
-        result=add(a,b)
+        result=add(num1,num2)
 
     elif (operation=="-"):
-        result=substraction(a,b)
+        result=substraction(num1,num2)
+
+    elif (operation=="*"):
+        result=multiply(num1,num2)
+
+    elif (operation=="/"):
+        result=division(num1,num2)
     
     else:
-        print("Invalid operation. Enter again.\n")
+        print("Invalid number. Enter again.\n")
 
-    print(result,"\n")
+    print(f"{num1} {operation} {num2} = {result} \n")
